8932daa009870f9cae5f182d71ee7d4608a2ccc1
Pridelory <wangmeng.pantera@gmail.com>
2019-03-06 23:52:32 +0800

    equals和hashCode

---
+++ src/com/javaSE/HashCodeAndEquals/Person.java
@@ -0,0 +1,64 @@
+package com.javaSE.HashCodeAndEquals;
+
+import java.util.Objects;
+
+/**
+ * 为了验证equals和hashCode
+ */
+public class Person {
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    //覆写equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && this.getName().equals(person.getName());
+    }
+
+    //覆写hashCode
+    public int hashCode() {
+        return age * name.hashCode();
+    }
+}
---
+++ src/com/javaSE/HashCodeAndEquals/TestUsage.java
@@ -0,0 +1,40 @@
+package com.javaSE.HashCodeAndEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class TestUsage {
+
+    @Test
+    public void test1() {
+        Person p1 = new Person("王蒙", 19);
+        Person p2 = new Person("王蒙", 19);
+        //明显可见比较的是p1和p2的地址
+        System.out.println(p1 == p2);
+        //true
+        System.out.println(p1 == p1);
+        //如果Person没有覆写equals()，则调用Object里的equals(),实际上还是用"=="比较的
+        //当Person覆写equals()时，则根据比较规则来判定了
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.getAge() == p2.getAge());
+        System.out.println("----------------------");
+        //Integer比较测试
+        Integer i1 = 122;
+        Integer i2 = 122;
+        System.out.println(i1 == i2);
+    }
+
+    @Test
+    public void test2() {
+        HashSet<Person> hashSet = new HashSet<>();
+        hashSet.add(new Person("小明", 18));
+        hashSet.add(new Person("小明", 18));
+
+        Iterator<Person> iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
